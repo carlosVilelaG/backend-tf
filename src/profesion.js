a8056067d4d9b7322151784ea55e747fdb2beeec
carlosVilelaG <cvilela@uoc.edu>
2023-11-07 21:58:54 -0500

    Se agrega consulta de profesiones

--- src/profesion.js
+++ src/profesion.js
@@ -5,11 +5,10 @@ const router = express.Router();
 router.get('/profesion', async (req, res) => {
 
     const connection = await database.getConnection();
-    const estado = "ACTVO"
-    const query = "SELECT * FROM man_location_work.area_profesion WHERE estado=?";
+    const estado = "ACTIVO";
+    const query = "SELECT * FROM man_location_work.area_profesion where estado = ? ";
     const result = await connection.query(query, [estado]);    
     res.json(result);
 });
-
 
 module.exports = router;
