681cb1cf4d25c8346800623330f87fdb2e81a1be
CarlosVilela <cvilela@uoc.edu>
2023-12-03 23:50:09 -0500

    Se agrega endpoint de actualizar ya agregar ubicaciones

--- src/ubicaciones.js
+++ src/ubicaciones.js
@@ -13,5 +13,34 @@ router.get('/usuario/ubicacion/:email', async (req, res) => {
     res.json(result[0]);
 });
 
+// Endpoint para crear una nueva ubicación
+router.post('/usuario/ubicacion', async (req, res) => {
+    try {
+        const { identificacion_usuario, descripcion, latitud, longitud, telefono } = req.body;
+        const connection = await database.getConnection();
+
+        // Verificar si la ubicación ya existe
+        const query = "SELECT * FROM man_location_work.ubicacion WHERE identificacion_usuario = ?";
+        const existing = await connection.query(query, [identificacion_usuario]);
+
+        console.log('Se encontraron ',existing.length ,' registros con identificacion_usuario=',identificacion_usuario);
+        if (existing.length > 0) {
+            // Actualizar ubicación existente
+            const updateQuery = "UPDATE man_location_work.ubicacion SET descripcion = ?, latitud = ?, longitud = ?, telefono = ? WHERE identificacion_usuario = ?";
+            await connection.query(updateQuery, [descripcion, latitud, longitud, telefono, identificacion_usuario]);
+            console.log('Ubicación actualizada con éxito');
+            res.json({ message: 'Ubicación actualizada con éxito' });
+        } else {
+            // Insertar nueva ubicación
+            const insertQuery = "INSERT INTO man_location_work.ubicacion (identificacion_usuario, descripcion, latitud, longitud, telefono,estado) VALUES (?, ?, ?, ?, ?, ?)";
+            await connection.query(insertQuery, [identificacion_usuario, descripcion, latitud, longitud, telefono,"ACTIVO"]);
+            console.log('Ubicación creada con éxito');
+            res.status(201).json({ message: 'Ubicación creada con éxito' });
+        }
+    } catch (error) {
+        console.error('Error en la operación de ubicación:', error);
+        res.status(500).json({ message: 'Error en la operación de ubicación' });
+    }
+});
 
 module.exports = router;
